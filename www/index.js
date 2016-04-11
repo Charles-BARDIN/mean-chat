af4197a07e956372e66fa6781f882cd26f7c52ea
Charles BARDIN <bardincharles@gmail.com>
2016-04-11 19:32:33 +0200

    Adding angular-material-icons

--- www/index.js
+++ www/index.js
@@ -2,9 +2,10 @@ import angular from 'angular';
 import appModule from 'config';
 import ngSanitize from 'angular-sanitize';
 import ngMaterial from 'angular-material';
+import ngMdIcons from 'angular-material-icons';
 import 'css/master.scss';
 
-angular.bootstrap(document, [appModule.name, ngSanitize, ngMaterial]);
+angular.bootstrap(document, [appModule.name, ngSanitize, ngMaterial, ngMdIcons]);
 
 /**
  * Created by Charles on 02/04/2016.
